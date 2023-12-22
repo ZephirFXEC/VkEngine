dd9c5c934534c6a93440850ee2945089ffd067d6
ZephirFXEC <zephirfx.pro@gmail.com>
2023-12-22 12:12:37 +0100

    improved memory allocation for vertex buffers

--- src/vkEngineModel.cpp
+++ src/vkEngineModel.cpp
@@ -31,8 +31,7 @@ std::vector<VkVertexInputAttributeDescription> VkEngineModel::Vertex::getAttribu
 {
 	std::vector<VkVertexInputAttributeDescription> attributeDescriptions{
 		{0, 0, VK_FORMAT_R32G32_SFLOAT, offsetof(Vertex, mPosition)},
-		{1, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, mColor)}}
-	;
+		{1, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, mColor)}};
 
 	return attributeDescriptions;
 }
@@ -55,17 +54,40 @@ void VkEngineModel::createVertexBuffers(const std::vector<Vertex>& vertices)
 	mVertexCount = static_cast<uint32_t>(vertices.size());
 	assert(mVertexCount >= 3 && "Vertex count must be at least 3");
 
-	const VkDeviceSize bufferSize = sizeof(vertices[0]) * mVertexCount;
+	const VkBufferCreateInfo bufferInfo{
+		.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+		.size = sizeof(vertices[0]) * vertices.size(),
+		.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+		.sharingMode = VK_SHARING_MODE_EXCLUSIVE,
+	};
 
-	mDevice.createBuffer(bufferSize,
-						 VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
-						 VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-						 pVertexBuffer,
-						 pVertexBufferMemory);
+	if(vkCreateBuffer(mDevice.device(), &bufferInfo, nullptr, &pVertexBuffer) != VK_SUCCESS)
+	{
+		throw std::runtime_error("failed to create vertex buffer!");
+	}
+
+	VkMemoryRequirements memRequirements;
+	vkGetBufferMemoryRequirements(mDevice.device(), pVertexBuffer, &memRequirements);
+
+	const VkMemoryAllocateInfo allocInfo
+	{
+		.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
+		.allocationSize = memRequirements.size,
+		.memoryTypeIndex = mDevice.findMemoryType(memRequirements.memoryTypeBits,
+												  VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+													  VK_MEMORY_PROPERTY_HOST_COHERENT_BIT),
+	};
+
+	if(vkAllocateMemory(mDevice.device(), &allocInfo, nullptr, &pVertexBufferMemory) != VK_SUCCESS)
+	{
+		throw std::runtime_error("failed to allocate vertex buffer memory!");
+	}
+
+	vkBindBufferMemory(mDevice.device(), pVertexBuffer, pVertexBufferMemory, 0);
 
 	void* data = nullptr;
-	vkMapMemory(mDevice.device(), pVertexBufferMemory, 0, bufferSize, 0, &data);
-	memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
+	vkMapMemory(mDevice.device(), pVertexBufferMemory, 0, bufferInfo.size, 0, &data);
+	memcpy(data, vertices.data(), (size_t)bufferInfo.size);
 	vkUnmapMemory(mDevice.device(), pVertexBufferMemory);
 }
 } // namespace vke
