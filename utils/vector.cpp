b417f2c60348275b0db5fe46c21fbb76adf12efd
ZephirFXEC <zephirfx.pro@gmail.com>
2023-12-25 18:28:17 +0100

    custom vector container

---
+++ utils/vector.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Enzo Crema on 25/12/2023.
+//
+
+#include "vector.hpp"
+
+namespace vke {}  // namespace vke
---
+++ utils/vector.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Enzo Crema on 25/12/2023.
+//
+
+#ifndef VKENGINE_VECTOR_HPP
+#define VKENGINE_VECTOR_HPP
+
+namespace vke {
+
+class vector {};
+
+}  // namespace vke
+
+#endif  // VKENGINE_VECTOR_HPP
